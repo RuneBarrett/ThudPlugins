a31e2ced1e19b7aae1d9c604acdb3e3000a38ced
Rune Barrett <runebarrett@gmail.com>
2017-03-09 21:15:24 +0100

    added animations

--- DirectionLines/DirectionLinesPlugin.cs
+++ DirectionLines/DirectionLinesPlugin.cs
@@ -10,15 +10,26 @@ namespace Turbo.Plugins.RuneB
         public int TextDistanceAway { get; set; }
         public IFont TextFont { get; set; }
         public IBrush GreyBrush { get; set; }
+
         public float StrokeWidth { get; set; }
         public float HitRange { get; set; }
         public float CloseEnoughRange { get; set; }
+        public bool Debug { get; set; }
         public bool ShowText { get; set; }
         public bool ShowMonsterLines { get; set; }
+        public bool ShowGizmos { get; set; }
         public Dictionary<ActorRarity, IBrush> MonsterBrushes { get; set; }
         public Dictionary<GizmoType, GizmoLine> GizmoBrushes { get; set; }
 
         private IScreenCoordinate center { get { return Hud.Game.Me.ScreenCoordinate; } }
+
+
+
+        private float _opacityMod = 0.01f;
+        private float _opacity = 0.01f;
+        private float _lineWidthMod = 0.01f;
+        private float _lineWidth = 0.01f;
+        private bool _started;
 
         public DirectionLinesPlugin()
         {
@@ -28,6 +39,8 @@ namespace Turbo.Plugins.RuneB
         public override void Load(IController hud)
         {
             base.Load(hud);
+            Debug = true;
+            ShowGizmos = true;
             ShowText = true;
             HitRange = 55;
             CloseEnoughRange = 15;
@@ -45,15 +58,20 @@ namespace Turbo.Plugins.RuneB
             MonsterBrushes.Add(ActorRarity.Unique, Hud.Render.CreateBrush(100, 200, 0, 150, 0));
 
             GizmoBrushes = new Dictionary<GizmoType, GizmoLine>();
-            GizmoBrushes.Add(GizmoType.PoolOfReflection, new GizmoLine(GizmoLine.AnimType.Blink, Hud.Render.CreateBrush(100, 250, 255, 0, 0)));
-            //GizmoBrushes.Add(GizmoType.HealingWell, new GizmoLine(GizmoLine.AnimType.Fade, Hud.Render.CreateBrush(100, 255, 0, 0, 0)));
-            GizmoBrushes.Add(GizmoType.Banner, new GizmoLine(GizmoLine.AnimType.Blink, Hud.Render.CreateBrush(100, 0, 255, 0, 0)));//for testing
+
 
         }
 
         public void PaintTopInGame(ClipState clipState)
         {
             if (clipState != ClipState.BeforeClip) return;
+            if (Debug && !_started)
+            {
+                _started = true;   
+                GizmoBrushes.Add(GizmoType.Portal, new GizmoLine(GizmoLine.AnimType.Fade, Hud.Render.CreateBrush(100, 250, 255, 0, 0)));
+                GizmoBrushes.Add(GizmoType.IdentifyAll, new GizmoLine(GizmoLine.AnimType.WidthMod, Hud.Render.CreateBrush(100, 0, 0, 255, 0)));
+                GizmoBrushes.Add(GizmoType.SharedStash, new GizmoLine(GizmoLine.AnimType.Blink, Hud.Render.CreateBrush(100, 0, 255, 0, 0)));
+            }
 
             //Monster lines
             if (ShowMonsterLines)
@@ -82,28 +100,38 @@ namespace Turbo.Plugins.RuneB
             }
 
             //Gizmo lines
-            var Gizmos = Hud.Game.Actors.Where(actor => GizmoBrushes.ContainsKey(actor.GizmoType));
-            foreach (var gizmo in Gizmos)
+            if (ShowGizmos)
             {
-                var gizmoPos = gizmo.FloorCoordinate.ToScreenCoordinate();
-                var start = PointOnLine(center.X, center.Y, gizmoPos.X, gizmoPos.Y, 60);
-                var end = PointOnLine(gizmoPos.X, gizmoPos.Y - 30, center.X, center.Y, 20);
-
-                var gizmoLine = GizmoBrushes[gizmo.GizmoType];
-                switch (gizmoLine.anim)
+                var Gizmos = Hud.Game.Actors.Where(actor => GizmoBrushes.ContainsKey(actor.GizmoType));
+                foreach (var gizmo in Gizmos)
                 {
-                    case GizmoLine.AnimType.None:
-                        gizmoLine.brush.DrawLine(start.X, start.Y, end.X, end.Y, StrokeWidth * 0.8f);
-                        break;
-                    case GizmoLine.AnimType.Blink:
-                        //TextFont.DrawText(""+(Hud.Game.CurrentRealTimeMilliseconds/1000) % 2, end);
-                        if ((Hud.Game.CurrentRealTimeMilliseconds / 700) % 2 == 0)
+                    var gizmoPos = gizmo.FloorCoordinate.ToScreenCoordinate();
+                    var start = PointOnLine(center.X, center.Y, gizmoPos.X, gizmoPos.Y, 60);
+                    var end = PointOnLine(gizmoPos.X, gizmoPos.Y - 30, center.X, center.Y, 20);
+
+                    var gizmoLine = GizmoBrushes[gizmo.GizmoType];
+                    switch (gizmoLine.anim)
+                    {
+                        case GizmoLine.AnimType.None:
                             gizmoLine.brush.DrawLine(start.X, start.Y, end.X, end.Y, StrokeWidth * 0.8f);
-                        break;
-                    case GizmoLine.AnimType.Fade:
-                        break;
-                    case GizmoLine.AnimType.WidthMod:
-                        break;
+                            break;
+                        case GizmoLine.AnimType.Blink:
+                            //TextFont.DrawText(""+(Hud.Game.CurrentRealTimeMilliseconds/1000) % 2, end);
+                            if ((Hud.Game.CurrentRealTimeMilliseconds / 700) % 2 == 0)
+                                gizmoLine.brush.DrawLine(start.X, start.Y, end.X, end.Y, StrokeWidth * 0.8f);
+                            break;
+                        case GizmoLine.AnimType.Fade:
+                            if (_opacity < 0 || _opacity > 1) { _opacityMod = -_opacityMod; }
+                            _opacity += _opacityMod;
+                            gizmoLine.brush.Opacity = _opacity;
+                            gizmoLine.brush.DrawLine(start.X, start.Y, end.X, end.Y, StrokeWidth * 0.8f);
+                            break;
+                        case GizmoLine.AnimType.WidthMod:
+                            if (_lineWidth < 0 || _lineWidth > 1.5f) { _lineWidthMod = -_lineWidthMod; }
+                            _lineWidth += _lineWidthMod;
+                            gizmoLine.brush.DrawLine(start.X, start.Y, end.X, end.Y, StrokeWidth * _lineWidth);
+                            break;
+                    }
                 }
             }
         }
@@ -123,9 +151,9 @@ namespace Turbo.Plugins.RuneB
     public class GizmoLine
     {
         public enum AnimType { Blink, Fade, WidthMod, None }
-        public AnimType anim;
-        public IBrush brush;
-        private AnimType blink;
+        public AnimType anim { get; set; }
+        public IBrush brush { get; set; }
+        private AnimType blink { get; set; }
 
         public GizmoLine(AnimType anim, IBrush brush)
         {
